7b2f6a5d46b312ec97f9526c21cb534fe91ba8ee
Riccardo Masserini <masserini.riccardo.studente@itispaleocapa.it>
2023-05-25 22:48:49 +0200

    Aggiungere i file di progetto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,390 @@
+﻿namespace FileCsv_Masserini2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button11 = new System.Windows.Forms.Button();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button12 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(28, 36);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Funzione 1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(28, 65);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(103, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Funzione 2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(28, 94);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(103, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Funzione 3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(137, 94);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(103, 23);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Funzione 3A";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(28, 123);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(103, 23);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Funzione 4";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(28, 152);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(103, 23);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "Funzione 5";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(28, 181);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(103, 23);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "Funzione 6";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(28, 282);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(103, 23);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "Funzione 7";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(28, 386);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(103, 23);
+            this.button9.TabIndex = 8;
+            this.button9.Text = "Funzione 8";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(28, 415);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(103, 23);
+            this.button10.TabIndex = 9;
+            this.button10.Text = "Funzione 9";
+            this.button10.UseVisualStyleBackColor = true;
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.LabelEdit = true;
+            this.listView1.Location = new System.Drawing.Point(246, 36);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(546, 91);
+            this.listView1.TabIndex = 10;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button11);
+            this.groupBox1.Controls.Add(this.textBox4);
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.textBox6);
+            this.groupBox1.Controls.Add(this.textBox5);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(149, 133);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(802, 128);
+            this.groupBox1.TabIndex = 11;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            this.groupBox1.Visible = false;
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(306, 99);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(108, 23);
+            this.button11.TabIndex = 12;
+            this.button11.Text = "Crea Record";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(398, 53);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 22);
+            this.textBox4.TabIndex = 9;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(272, 53);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 8;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(674, 53);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(100, 22);
+            this.textBox6.TabIndex = 11;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(544, 53);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(100, 22);
+            this.textBox5.TabIndex = 10;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(147, 53);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 7;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(21, 53);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 6;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(671, 26);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(109, 16);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Valore Booleano";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(541, 26);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(102, 16);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Valore Random";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(427, 26);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(36, 16);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Note";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(269, 26);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(103, 16);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Milioni di Kw/ora";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(170, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nazione";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(53, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Anno";
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(42, 15);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(100, 22);
+            this.textBox7.TabIndex = 12;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button12);
+            this.groupBox2.Controls.Add(this.textBox7);
+            this.groupBox2.Location = new System.Drawing.Point(149, 267);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(183, 76);
+            this.groupBox2.TabIndex = 13;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "groupBox2";
+            this.groupBox2.Visible = false;
+            // 
+            // button12
+            // 
+            this.button12.Location = new System.Drawing.Point(56, 43);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(75, 23);
+            this.button12.TabIndex = 13;
+            this.button12.Text = "Ricerca";
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1183, 450);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button12;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,339 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace FileCsv_Masserini2
+{
+    public partial class Form1 : Form
+    {
+        public Random r = new Random();
+        public string fileName = @"masserini1.csv";
+        public string fileName1 = @"masserini.csv";
+        public string n, anno, nazione, note,p;
+        public char de = ';';
+        public int LMn = 0, Ln = 0, tempo, VRandom, ad = 0, contatore = 0, i = 0, L;
+        public float MKwh;
+        public bool Vbooleano;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        #region Pulsanti
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Istruzione1();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Il numero di campi è: " + Istruzione2());
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Il numero di caratteri è: " + Istruzione3());
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            listView1.Clear();
+            int[] MaxC = new int[contatore];
+            MaxC = Istruzione3A();
+            for (int i = 0; i < contatore; i++)
+            {
+                listView1.Items.Add(MaxC[i].ToString());
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Istruzione4();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            groupBox1.Show();
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            anno = textBox1.Text;
+            nazione = textBox2.Text;
+            MKwh = float.Parse(textBox3.Text);
+            note = textBox4.Text;
+            VRandom = int.Parse(textBox5.Text);
+            Vbooleano = bool.Parse(textBox6.Text);
+            Istruzione5();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            groupBox2.Show();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            listView1.Clear();
+            Istruzione6();            
+        }
+        private void button12_Click(object sender, EventArgs e)
+        {
+            L = Istruzione7(textBox7.Text);
+            if (L != -1)
+            {
+                MessageBox.Show("Il tuo valore è stato trovato alla riga: " + L);
+            }
+            else
+            {
+                MessageBox.Show("La ricerca non ha avuto successo");
+            }
+        }
+
+
+        #endregion
+
+        #region funzioni di servizio
+        private void Istruzione1()
+        {
+            StreamWriter writer = new StreamWriter(fileName, append: false);
+            StreamReader reader = new StreamReader(fileName1);
+            n = reader.ReadLine();
+            while (n != null)
+            {
+                if (i == 0)
+                {
+                    writer.WriteLine(n + de + "Mio valore" + de + "Cancellazione Logica");
+                }
+                else
+                {
+                    int Nr = r.Next(10, 21);
+                    writer.WriteLine(n + de + Nr + de + "false");
+                }
+                i++;
+                n = reader.ReadLine();
+            }
+            writer.Close();
+            reader.Close();
+        }
+        private int Istruzione2()
+        {
+            StreamReader reader = new StreamReader(fileName);
+            n = reader.ReadLine();
+            reader.Close();
+            contatore = n.Split(';').Length;
+            return contatore;
+        }
+
+        private int Istruzione3()
+        {
+            StreamReader reader = new StreamReader(fileName);
+            n = reader.ReadLine();
+            while (n != null)
+            {
+                Ln = n.Length;
+                if (i != 0)
+                {
+                    if (LMn < Ln)
+                    {
+                        LMn = n.Length;
+                    }
+                }
+                i++;
+                n = reader.ReadLine();
+            }
+            reader.Close();
+            return LMn;
+        }
+
+        private int[] Istruzione3A()
+        {
+            StreamReader reader = new StreamReader(fileName);
+            n = reader.ReadLine();
+            int[] LMassima = new int[contatore];
+            n = reader.ReadLine();
+            while (n != null)
+            {
+                string[] split = n.Split(';');
+                string[] array = new string[contatore];
+                for (int i = 0; i < contatore; i++)
+                {
+                    reader.DiscardBufferedData();
+                    reader.BaseStream.Seek(0, SeekOrigin.Begin);
+                    n = reader.ReadLine();
+                    ad = 0;
+                    while (n != null)
+                    {
+                        string[] stringaSplit = n.Split(';');
+                        if (ad != 0)
+                        {
+                            if (LMassima[i] < stringaSplit[i].Length)
+                            {
+                                LMassima[i] = stringaSplit[i].Length;
+                            }
+                        }
+                        ad++;
+                        n = reader.ReadLine();
+                    }
+                }
+            }
+            reader.Close();
+            return LMassima;
+        }
+
+        private void Istruzione4()
+        {
+            StreamReader reader = new StreamReader(fileName);
+            StreamWriter writer = new StreamWriter("appoggio.csv");
+            n = reader.ReadLine();
+            while (n != null)
+            {
+                if (i != 0)
+                {
+                    writer.WriteLine(n.PadRight(70));
+                }
+                else
+                {
+                    writer.WriteLine(n);
+                }
+                i++;
+                n = reader.ReadLine();
+            }
+            reader.Close();
+            writer.Close();
+            File.Replace("appoggio.csv", fileName, "backup.csv");
+        }
+
+        private void Istruzione5()
+        {
+            StreamReader reader = new StreamReader(fileName);
+            StreamWriter writer = new StreamWriter("appoggio.csv");
+            n = reader.ReadLine();
+            while (n != null)
+            {
+                writer.WriteLine(n);
+                n = reader.ReadLine();
+            }
+            writer.WriteLine(anno + de + nazione + de + MKwh + de + note + de + VRandom + de + Vbooleano);
+            writer.Close();
+            reader.Close();
+            File.Replace("appoggio.csv", fileName, "backup.csv");
+        }
+
+        private void Istruzione6()
+        {
+            StreamReader reader = new StreamReader(fileName);
+            n = reader.ReadLine();
+            while (n != null)
+            {
+                String[] split = n.Split(';');
+                if (split[5] == "false")
+                {
+                    listView1.Items.Add(split[0] + de + split[1] + de + split[2]);
+                }
+                i++;
+                n = reader.ReadLine();
+            }
+            reader.Close();
+        }
+
+        private int Istruzione7(string p)
+        {
+            StreamReader reader = new StreamReader(fileName);
+            n = reader.ReadLine();
+            i = 0;
+            while (n != null)
+            {
+                String[] split1 = n.Split(';');
+                if (split1[2] == p)
+                {
+                    reader.Close();
+                    return i;
+                }
+                n = reader.ReadLine();
+                i++;
+            }
+            return -1;
+
+        }
+        private int Ricerca2()
+        {
+            StreamReader reader = new StreamReader(fileName);
+            i = 0;
+            n = reader.ReadLine();
+            while (n != null)
+            {
+                String[] split1 = n.Split(';');
+                String[] split2 = split1[Caratteri() - 1].Split(' ');
+                if (split1[2] == p)
+                {
+                    reader.Close();
+                    return i;
+                }
+                n = reader.ReadLine();
+                i++;
+            }
+            return -1;
+        }
+        private int Caratteri()
+        {
+           int contatore = 0;
+            StreamReader reader = new StreamReader(fileName);
+            n = reader.ReadLine();
+            reader.Close();
+            contatore = n.Split(';').Length;
+            return contatore;
+        }   
+        private void Istruzione8()
+        {
+            StreamReader reader = new StreamReader(fileName);
+            StreamWriter writer = new StreamWriter("appoggio.csv");
+            i = 0;
+            n = reader.ReadLine();
+            while(n != null)
+            {
+                if (i != 0)
+                {
+                    string[] split = n.Split(';');
+                    string[] split1 = split[Caratteri() - 1].Split(' ');
+                    int riga = int.Parse(split1[0]);
+                    if(riga == L)
+                    {
+                        writer.WriteLine(anno + de + nazione + de + MKwh + de + note + de + VRandom + de + Vbooleano);
+                        writer.WriteLine(n);
+                    }
+                    else
+                    {
+                        writer.WriteLine(n);
+                    }
+                }
+                else
+                {
+                    writer.WriteLine(n);
+                }
+                i++;
+                n = reader.ReadLine();
+            }
+            reader.Close();
+            writer.Close();
+            File.Replace("appoggio.csv", fileName, "backup.csv");
+        }
+        
+
+        #endregion
+    }
+}
